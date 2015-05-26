04f25a1bbd0d57ebaf109d2bf93db09f695846b6
Zachary Brachmanis <zac@kodigosystems.com>
2015-05-26 15:15:03 -0700

    Initial Commit

---
+++ Quadratic Formula Solver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quadratic Formula Solver
+//
+//  Created by Zachary Brachmanis on 5/26/15.
+//  Copyright (c) 2015 Zac Brachmanis Developments. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
