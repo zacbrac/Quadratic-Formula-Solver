89bdf997c361169ff30786baee4c948126be4a17
Zachary Brachmanis <zac@kodigosystems.com>
2015-05-26 15:49:46 -0700

    General stuff

--- Quadratic Formula Solver/ViewController.swift
+++ Quadratic Formula Solver/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var paramA: UITextField!
+    @IBOutlet weak var paramC: UITextField!
+    @IBOutlet weak var paramB: UITextField!
+
+    @IBOutlet weak var answerA: UILabel!
+    @IBOutlet weak var answerB: UILabel!
+    
+    @IBAction func doQuadratic(sender: AnyObject) {
+        var a: Double = (paramA.text as NSString).doubleValue
+        var b: Double = (paramB.text as NSString).doubleValue
+        var c: Double = (paramC.text as NSString).doubleValue
+        
+        var parta: Double = (-(b) + sqrt((b*b) - (4*a*c))) / (2*a)
+        var partb: Double = (-(b) - sqrt((b*b) - (4*a*c))) / (2*a)
+        
+        var firstAnswerString:String = String(format:"%.2f", parta)
+        var secondAnswerString:String = String(format:"%.2f", partb)
+        
+        answerA.text = firstAnswerString
+        answerB.text = secondAnswerString
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
